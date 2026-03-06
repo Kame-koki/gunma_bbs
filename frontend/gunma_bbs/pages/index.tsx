1bdbb72924fe08721c01ddfc0ba40c0a617f043e
Koki <raiden0911kame@gmail.com>
2026-03-07 00:00:14 +0900

    index.tsx create

--- frontend/gunma_bbs/pages/index.tsx
+++ frontend/gunma_bbs/pages/index.tsx
@@ -1,21 +1,50 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
+import { Post } from "./types";
+import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+type Props = {
+  posts: Post[];
+}
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3001/api/v1/posts");
+  const posts = await res.json();
 
-export default function Home() {
+  console.log(posts);
+
+  return{
+    props:{
+      posts,
+    },
+    revalidate: 60 * 60 * 24,
+  }
+}
+
+
+export default function Home({ posts }: Props) {
   return (
     <div>
-      <main>
-      </main>
+      <Head>
+        <title>群馬県の掲示板</title>
+        <meta name="description" content="自分の好きな群馬県をみんなに届ける" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />4
+      </Head>
+
+      <div className="">
+        <h2>群馬県の掲示板</h2>
+        <Link href="/create-post" className="">Create new Post</Link>
+        {posts.map((post: Post) => (
+          <div key={post.id} className="">
+            <Link href={`posts/${post.id}`} className="">
+              <h2>{post.title}</h2>
+            </Link>
+            <p>{post.content}</p>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
