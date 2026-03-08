4f69bbd2e2899e4b2e05245d8adf0d0b45a5e4ec
Koki <raiden0911kame@gmail.com>
2026-03-08 22:53:10 +0900

    index add css

--- frontend/gunma_bbs/pages/index.tsx
+++ frontend/gunma_bbs/pages/index.tsx
@@ -29,18 +29,19 @@ export default function Home({ posts }: Props) {
         <meta name="description" content="自分の好きな群馬県をみんなに届ける" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />4
       </Head>
-
-      <div className="">
-        <h2>群馬県の掲示板</h2>
-        <Link href="/create-post" className="">Create new Post</Link>
+      <div className="pageContainer">
+        <div className="titleContainer">
+          <h2>群馬県の掲示板</h2>
+        </div>
+        <Link href="/create-post" className="createButton">Create new Post</Link>
         {posts.map((post: PostParams) => (
           <div key={post.id} className="">
             <Link href={`posts/${post.id}`} className="">
               <h2>{post.title}</h2>
             </Link>
             <p>{post.content}</p>
-            <button>Edit</button>
-            <button>Delete</button>
+            <button>編集</button>
+            <button>削除</button>
           </div>
         ))}
       </div>
